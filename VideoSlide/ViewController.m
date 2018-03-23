47a9df0d80b17a0237d8262207bf54c11d57d79d
nyzcd <zchangwen@sina.cn>
2018-03-23 16:53:20 +0800

    update

--- VideoSlide/ViewController.m
+++ VideoSlide/ViewController.m
@@ -7,22 +7,72 @@
 //
 
 #import "ViewController.h"
+#import "PlayerViewController.h"
 
-@interface ViewController ()
+#import <MediaPlayer/MediaPlayer.h>
+#import <MobileCoreServices/MobileCoreServices.h>
+#import <AVFoundation/AVFoundation.h>
+
+
+@interface ViewController ()<UIImagePickerControllerDelegate,UINavigationControllerDelegate>
+
+@property(nonatomic,strong) UIImagePickerController *imagePickerController;
+@property(nonatomic,strong) NSURL *path;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.imagePickerController = [[UIImagePickerController alloc] init];
+    _imagePickerController.delegate = self;
+    _imagePickerController.modalTransitionStyle = UIModalTransitionStyleFlipHorizontal;
+    _imagePickerController.allowsEditing = YES;
+    
+    
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+    [btn setBackgroundColor:[UIColor blackColor]];
+    [btn setTitle:@"编辑视频" forState:UIControlStateNormal];
+    [btn setTitleColor:[UIColor yellowColor] forState:UIControlStateNormal];
+    [btn setFrame:CGRectMake(50, 100, 100, 50)];
+    [btn addTarget:self action:@selector(pressEditing) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:btn];
 }
 
 
-- (void)didReceiveMemoryWarning {
+- (void)pressEditing
+{
+    self.imagePickerController.sourceType = UIImagePickerControllerSourceTypeSavedPhotosAlbum;
+    _imagePickerController.mediaTypes = [NSArray arrayWithObjects:@"public.movie", nil];
+    [self presentViewController:_imagePickerController animated:YES completion:nil];
+}
+
+#pragma mark UIImagePickerControllerDelegate
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info{
+    
+    NSString *mediaType=[info objectForKey:UIImagePickerControllerMediaType];
+    
+    if (![mediaType isEqualToString:(NSString *)kUTTypeImage])
+    {
+        _path = info[UIImagePickerControllerMediaURL];
+        [self playView];
+        [self dismissViewControllerAnimated:YES completion:nil];
+    }
+}
+
+- (void)playView
+{
+    PlayerViewController *vc = [[PlayerViewController alloc] init];
+//    pvc.videoUrl = _videoUrl;
+    [self.navigationController pushViewController:vc animated:YES];
+}
+
+- (void)didReceiveMemoryWarning
+{
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 
