b1ad0a1a3cc7ddd61ab783f7d8d86f0a0d02ef05
nyzcd <zchangwen@sina.cn>
2018-03-23 16:22:38 +0800

    初始化

---
+++ VideoSlide/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VideoSlide
+//
+//  Created by ken on 2018/3/23.
+//  Copyright © 2018年 zchangwen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VideoSlide/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VideoSlide
+//
+//  Created by ken on 2018/3/23.
+//  Copyright © 2018年 zchangwen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
